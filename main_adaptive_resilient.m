e5b732ceae5465cb856513ea0c022f87c426a78b
raaslab <lovetuliper@gmail.com>
2021-02-02 21:37:35 -0500

    add the main file

---
+++ main_adaptive_resilient.m
@@ -0,0 +1,8 @@
+% MCTS for adaptive and resilient target tracking
+% Compare it with minmax tree seach, min tree search without attacks (small case), 
+% MCTS without attacks and random algorithm (a larger case)
+
+clc;
+close all; 
+
+
